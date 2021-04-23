12fa8290b3267ab043d2d5ef44e112802eb56877
Oriana Laura K González <orianalk@gmail.com>
2021-04-23 06:59:45 +0000

    Add comment for routes

--- server.py
+++ server.py
@@ -4,5 +4,10 @@ from flask import (Flask, render_template, request, flash, session, redirect)
 
 app = Flask(__name__)
 
+
+# app routes go here
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
