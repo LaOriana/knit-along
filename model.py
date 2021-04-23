32f0ecc64f1e0e0dd8f1f1b9735c647945d5d8b3
Oriana Laura K González <orianalk@gmail.com>
2021-04-23 12:52:35 +0000

    Complete Event class and add __repr__ to Event and User classes

--- model.py
+++ model.py
@@ -16,15 +16,38 @@ class User(db.Model):
     user_id = db.Column(db.Integer, 
                         primary_key=True, 
                         autoincrment=True, 
-                        nullable=False)
+                        nullable=False
+                        )
     username = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String, nullable=False)
     image = db.Column(db.String, nullable=True)
 
+    def __repr__(self):
+        return f'<User user_id={self.user_id} username={username} email={email}>'
+
 
 class Event(db.Model):
     """An event."""
+
+    __tablename__ = 'events'
+
+    # Is db.Date() correct?
+    # Pattern - Is string the correct usage for the API link
+    event_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrment=True,
+                        nullable=False
+                        )
+    event_name = db.Column(db.String(128), nullable=False)
+    start_date = db.Column(db.Date(), nullable=False)
+    end_date = db.Column(db.Date(), nullable=False)
+    pattern = db.Column(db.String, nullable=False)
+    # Do I need my chat forum running before I can add this?
+    # chat = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Event = event_id{event_id} event_name{event_name} start_date={start_date} end_date={end_date} pattern={pattern} >'
 
 
 class EventOwner(db.Model):
