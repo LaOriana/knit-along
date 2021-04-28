59677aacc3b0ab91aab35bdb3ee993888673d983
Oriana Laura K González <orianalk@gmail.com>
2021-04-28 11:08:05 +0000

    Add and complete create_event function in crud.py

--- crud.py
+++ crud.py
@@ -3,7 +3,7 @@
 from model import db, User, Event, EventOwner, EventAttended, Post, connect_to_db
 
 # add images to static file
-def create_user(username, email, password, image)
+def create_user(username, email, password, image):
     """Create and return a new user."""
 
     user = User(username=username, email=email, password=password, image=image)
@@ -12,3 +12,13 @@ def create_user(username, email, password, image)
     db.session.commit()
 
     return user
+
+def create_event(event_name, start_date, end_date, pattern):
+    """Create and return a new event."""
+
+    event = Event(event_name=event_name, start_date=start_date, end_date=end_date, pattern=pattern)
+
+    db.session.add(event)
+    db.session.commit()
+
+    return event
