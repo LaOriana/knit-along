8198f9209364774b6db505a1368532f77c39abca
Oriana <oriana@Casiopeas-MacBook-Pro.local>
2021-06-04 11:20:54 -0700

    Add routes for homepage, signup, login, logout

--- server.py
+++ server.py
@@ -6,9 +6,33 @@ import model
 
 app = Flask(__name__)
 
+@app.route('/')
+def hompage():
+    """View homepage."""
+
+    return render_template('homepage.html')
+
+# @app.route('/signup')
+# def signup():
+#     """Signup user."""
+
+# @app.route('/login')
+# def user_login():
+#     """Login user."""
+
+# @app.route('/logout')
+# def user_login():
+#     """Login user."""
 
 # app routes go here
-
+# Create account
+# Login
+# Account
+# Homepage
+# Bookshelf
+# Create Event
+# Event Info
+# Forum
 
 
 if __name__ == '__main__':
